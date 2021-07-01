6d650910029d36a9b0f3a424d913f5b934e2d9d9
Josh <joshua.foster@earthware.co.uk>
2021-07-01 17:13:38 +0100

    Aligned container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
 import Grid from './components/Organisms/Grid/Grid';
+
+const Wrapper = styled(Container)`
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
   return (
     <div className="App">
       <main>
-        <Container>
+        <Wrapper>
           <Grid width={20} height={20} />
-        </Container>
+        </Wrapper>
       </main>
     </div>
   );
